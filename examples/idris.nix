9d9e008fabaa74f48a3c629fb5a51b5b4d12c624
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-07-05 23:16:51 -0400

    init

--- examples/idris.nix
+++ examples/idris.nix
@@ -3,10 +3,11 @@ build-idris-package {
   name = "yaml";
   version = "2018-01-25";
 
-  # This is the .ipkg file that should be built, defaults to the package name
-  # In this case it should build `Yaml.ipkg` instead of `yaml.ipkg`
-  # This is only necessary because the yaml packages ipkg file is
-  # different from its package name here.
+  /* This is the .ipkg file that should be built, defaults to the package name
+     In this case it should build `Yaml.ipkg` instead of `yaml.ipkg`
+     This is only necessary because the yaml packages ipkg file is
+     different from its package name here.
+  */
   ipkgName = "Yaml";
   # Idris dependencies to provide for the build
   idrisDeps = [ contrib lightyear ];
