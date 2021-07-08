7a377386a881cc4d6b8ea69fc2dae299eb17a328
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-07-07 22:11:16 -0400

    wip: basic on saving working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import * as Ap from "fp-ts/Apply";
+import * as Eq from "fp-ts/Eq";
 import * as O from "fp-ts/Option";
+import * as S from "fp-ts/Set";
 import { pipe } from "fp-ts/function";
 import * as D from "io-ts/Decoder";
 
 import { formatDocument } from "./commands/formatDocument";
+import { isFalse } from "./typeGuards";
 import { ExtensionSettings } from "./types";
 
 /*
@@ -36,9 +40,13 @@ const getSettings = (): ExtensionSettings => ({
     O.getOrElse(() => ""),
   ),
   formatOnSave: pipe(
-    O.fromNullable(nova.workspace.config.get(ExtensionConfigKeys.FormatOnSave)),
-    O.alt(() => O.fromNullable(nova.config.get(ExtensionConfigKeys.FormatOnSave))),
-    O.chain((value) => O.fromEither(D.boolean.decode(value))),
+    Ap.sequenceT(O.option)(
+      O.fromEither(D.boolean.decode(nova.workspace.config.get(ExtensionConfigKeys.FormatOnSave))),
+      O.fromEither(D.boolean.decode(nova.config.get(ExtensionConfigKeys.FormatOnSave))),
+    ),
+    O.map(
+      ([workspaceFormatOnSave, globalFormatOnSave]) => workspaceFormatOnSave || globalFormatOnSave,
+    ),
     O.getOrElseW(() => false),
   ),
 });
@@ -49,6 +57,13 @@ const invokeFormatDocumentCommand = (settings: ExtensionSettings) => (editor: Te
 /*
  * Main
  */
+
+const saveListeners: Set<TextEditor> = new Set();
+
+const eqTextEditor: Eq.Eq<TextEditor> = {
+  // Since editors have no unique identifier exposed, we have to resort to a nested property on another type
+  equals: (e1, e2) => Str.Eq.equals(e1.document.uri, e2.document.uri),
+};
 
 export const activate = () => {
   console.log("Activating...");
@@ -78,6 +93,15 @@ export const activate = () => {
   });
 
   nova.commands.register(ExtensionConfigKeys.FormatDocument, invokeFormatDocumentCommand(settings));
+
+  nova.workspace.onDidAddTextEditor((editor: TextEditor): void => {
+    if (isFalse(settings.formatOnSave)) return;
+
+    editor.onWillSave(invokeFormatDocumentCommand(settings));
+
+    // keep track of editors to later remove if formatOnSave is disabled while plugin is activated
+    pipe(saveListeners, S.insert(eqTextEditor)(editor));
+  });
 };
 
 export const deactivate = () => {
