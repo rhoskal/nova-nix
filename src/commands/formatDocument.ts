c8e480be1e75c1465d83fe44bfe12d98f874c1c4
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-07-18 14:19:28 -0400

    remove unnecessary TE.sequence

--- src/commands/formatDocument.ts
+++ src/commands/formatDocument.ts
@@ -1,6 +1,5 @@
 import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
-import { pipe } from "fp-ts/function";
 import { match } from "ts-pattern";
 
 /*
@@ -19,26 +18,22 @@ interface InvokeFormatterError {
 export const formatDocument = (editor: TextEditor, formatterPath: string) => {
   const documentPath = editor.document.path;
 
-  const safeFormat = pipe(
-    TE.sequenceSeqArray<void, InvokeFormatterError>([
-      TE.tryCatch<InvokeFormatterError, void>(
-        () => {
-          return new Promise<void>((resolve, reject) => {
-            const process = new Process("/usr/bin/env", {
-              args: [`${formatterPath}`, `${documentPath}`],
-            });
+  const safeFormat = TE.tryCatch<InvokeFormatterError, void>(
+    () => {
+      return new Promise<void>((resolve, reject) => {
+        const process = new Process("/usr/bin/env", {
+          args: [`${formatterPath}`, `${documentPath}`],
+        });
 
-            process.onDidExit((status) => (status === 0 ? resolve() : reject()));
+        process.onDidExit((status) => (status === 0 ? resolve() : reject()));
 
-            process.start();
-          });
-        },
-        () => ({
-          _tag: "invokeFormatterErrror",
-          reason: "Failed to format the document. This is likely a bug with nixfmt.",
-        }),
-      ),
-    ]),
+        process.start();
+      });
+    },
+    () => ({
+      _tag: "invokeFormatterErrror",
+      reason: "Failed to format the document. This is likely a bug with nixfmt.",
+    }),
   );
 
   safeFormat().then(
